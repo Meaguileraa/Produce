1765647f1311e97a20d56bea1cde88fb7684352a
vagrant <vagrant@vagrant.vm>
2020-07-09 05:07:56 +0000

    Final Code with comments

--- produce_summary.py
+++ produce_summary.py
@@ -1,45 +1,23 @@
 
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def deliveries(day, file): #Defining a function with two parameters
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    delivery_files = open(file) #Variable that opens the external files
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print("Day", day) #prints what day it is
 
+    for line in delivery_files: #for loop going through each line of each file
+        line = line.rstrip() # removes characters from line
+        words = line.split('|') # splits each line where there is a "|" into a word
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        melon = words[0] # melon is a variable for the first word in words
+        count = words[1] #count is a variable for the second word in words 
+        amount = words[2] #amount is a variable for the third word in words
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
+        #print statement that is formatted 
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    delivery_files.close() #closing file
 
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
+deliveries(1, "um-deliveries-20140519.txt") #calling the function with both arguments
+deliveries(2, "um-deliveries-20140520.txt") #calling the function with both arguments
+deliveries(3, "um-deliveries-20140521.txt") #calling the function with both arguments
